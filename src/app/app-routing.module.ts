39b1012d34efada7b693b8c50ca3476e2d091137
Mehmet <mehmetali_1222@hotmail.com>
2022-10-19 19:20:22 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BackgroundComponent } from './background/background.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    title: 'First component',
+    component: BackgroundComponent, // this is the component with the <router-outlet> in the template
+    // children: [
+    //   {
+    //     path: 'child-a', // child route path
+    //     component: ChildAComponent, // child route component that the router renders
+    //   },
+    //   {
+    //     path: 'child-b',
+    //     component: ChildBComponent, // another child route component that the router renders
+    //   },
+    // ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
+import { WorkexperienceComponent } from './workexperience/workexperience.component';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    ContactsComponent,
+    AboutComponent,
+    WorkexperienceComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
