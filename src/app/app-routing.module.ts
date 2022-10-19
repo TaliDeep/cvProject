3b7e63397d72e13d4ce4c10e4db0d2099eba83f7
Mehmet <mehmetali_1222@hotmail.com>
2022-10-19 19:34:48 +0200

    d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { BackgroundComponent } from './background/background.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    title: 'First component',
-    component: BackgroundComponent, // this is the component with the <router-outlet> in the template
-    // children: [
-    //   {
-    //     path: 'child-a', // child route path
-    //     component: ChildAComponent, // child route component that the router renders
-    //   },
-    //   {
-    //     path: 'child-b',
-    //     component: ChildBComponent, // another child route component that the router renders
-    //   },
-    // ],
-  },
 ];
 
 @NgModule({
