71a633008fb92758fe76be2ac891515913732072
Mehmet <mehmetali_1222@hotmail.com>
2022-10-21 23:52:03 +0200

    test

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -10,6 +10,47 @@ export class ContactsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.changecolorOfMail();
+    this.changecolorOfNumber();
+    this.changecolorOfAddress();
   }
 
+  private changecolorOfMail () {
+    var first = document.getElementById('description-mail');
+    var sec = document.getElementsByClassName('fa')[0] as HTMLElement;
+    if (sec != null) {
+      first.onmouseover = () => {
+        sec.style.color = '#2196F3';  
+      }
+      first.onmouseleave = () => {
+        sec.style.color = 'white';  
+      }
+    }
+  }
+
+  private changecolorOfNumber () {
+    var first = document.getElementById('description-number');
+    var sec = document.getElementsByClassName('fa')[1] as HTMLElement;
+    if (sec != null) {
+      first.onmouseover = () => {
+        sec.style.color = '#2196F3';  
+      }
+      first.onmouseleave = () => {
+        sec.style.color = 'white';  
+      }
+    }
+  }
+
+  private changecolorOfAddress () {
+    var first = document.getElementById('description-address');
+    var sec = document.getElementsByClassName('fa')[2] as HTMLElement;
+    if (sec != null) {
+      first.onmouseover = () => {
+        sec.style.color = '#2196F3';  
+      }
+      first.onmouseleave = () => {
+        sec.style.color = 'white';  
+      }
+    }
+  }
 }
