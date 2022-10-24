2bb86d95a5903c597aaa6069a57278d2028d4420
Mehmet <mehmetali_1222@hotmail.com>
2022-10-24 14:49:59 +0200

    test

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -5,18 +5,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
 export class NavBarComponent implements OnInit {
-
-  private showTopBar: any = true;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.showTopBar = window.innerWidth;
+    window.addEventListener('scroll', this.scroll, true)
   }
 
   public scrollToTop () {
     document.documentElement.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 
+  }
+
+  scroll = (): void => {
+    
+    if(window.scrollY === 0){
+      document.body.style.setProperty('--navbar-color', "#100e17");
+    }else if(window.scrollY > 0){
+      document.body.style.setProperty('--navbar-color', "#202026");
+    }
+
+    if(window.scrollY >= 0 && window.scrollY <= 658) {
+      var element = <HTMLInputElement> document.getElementById("home");
+      if(!element.checked) {
+        element.checked = true;
+      }
+    }
+
+    if(window.scrollY >= 389 && window.scrollY <= 659) {
+      var element = <HTMLInputElement> document.getElementById("about");
+      if(!element.checked) {
+        element.checked = true;
+      }
+    }
   }
 }
